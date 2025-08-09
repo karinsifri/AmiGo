ed1ae366d71d5788b300d7cfcf6a68569a64cf18
Karin Sifri <karin.sifri@gmail.com>
2025-08-09 21:03:58 +0300

    create data classes for loop folding

--- src/instructions/loop_folding.py
+++ src/instructions/loop_folding.py
@@ -1,2 +1,28 @@
+from __future__ import annotations
+from dataclasses import dataclass
+
+
+@dataclass
+class Instructions:
+    content: list[Loop | str]
+
+    def __len__(self) -> int:
+        return len(self.content)
+
+    def __str__(self) -> str:
+        return ",".join(map(str, self.content))
+
+
+@dataclass
+class Loop:
+    num_repetitions: int
+    content: Instructions
+
+    def __str__(self) -> str:
+        if len(self.content) == 1:
+            return f"{self.num_repetitions}{self.content}"
+        return f"{self.num_repetitions}*[{self.content}]"
+
+
 def fold_loops():
     pass
