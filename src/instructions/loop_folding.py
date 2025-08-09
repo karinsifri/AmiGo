7df6d9a7ac217bfc26bebd55ff72e63c14029e27
Karin Sifri <karin.sifri@gmail.com>
2025-08-09 21:16:51 +0300

    copy function from the notebook

--- src/instructions/loop_folding.py
+++ src/instructions/loop_folding.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 
 
 @dataclass
-class Instructions:
+class Program:
     content: list[Loop | str]
 
     def __len__(self) -> int:
@@ -16,7 +16,7 @@ class Instructions:
 @dataclass
 class Loop:
     num_repetitions: int
-    content: Instructions
+    content: Program
 
     def __str__(self) -> str:
         if len(self.content) == 1:
@@ -24,5 +24,42 @@ class Loop:
         return f"{self.num_repetitions}*[{self.content}]"
 
 
-def fold_loops():
-    pass
+def fold_loops(instructions: list[str]) -> list[Loop|str]:
+    n = len(instructions)
+
+    if n == 1:
+        return instructions
+
+    for window_size in range(n // 2, 0, -1):
+        i = 0
+        while i <= n - 2 * window_size:
+            pattern = instructions[i:i + window_size]
+            if len(set(pattern)) == 1:
+                i += 1
+                continue
+
+            count = 1
+            while (i + count * window_size + window_size) <= n and \
+                    instructions[i + count * window_size: i + (count + 1) * window_size] == pattern:
+                count += 1
+
+            if count > 1:
+                folded = []
+                # Fold BEFORE
+                folded.extend(fold_loops(instructions[:i]))
+
+                # Fold INSIDE (in case the pattern itself can be folded further)
+                inner_folded = fold_loops(pattern)
+
+                # Fold the loop
+                if len(inner_folded) == 1 and isinstance(inner_folded[0], tuple):
+                    folded.append((count * inner_folded[0][0], inner_folded[0][1]))
+                else:
+                    folded.append((count, inner_folded))
+
+                # Fold AFTER
+                folded.extend(fold_loops(instructions[i + count * window_size:]))
+                return folded
+
+            i += 1
+    return instructions
--- test/test_loop_folding.py
+++ test/test_loop_folding.py
@@ -6,8 +6,10 @@ from src.instructions.loop_folding import fold_loops
 @pytest.mark.parametrize("instructions, folded_instructions", [
     ('a', 'a'),
     ('a a', '2a'),
-    ('a a b a a b', '2*[2a,b]')
+    ('a a b a a b', '2*[2a,b]'),
+    ('a b a b a b a b', '4*[a,b]'),
 ])
 def test_loop_folding(instructions, folded_instructions):
     """ Test the fold_loops function """
-    assert fold_loops(instructions) == folded_instructions
+    actual = ",".join(map(str, fold_loops(instructions.split())))
+    assert actual == folded_instructions
