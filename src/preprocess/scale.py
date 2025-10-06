604f84171c8b69f65bb35b66ea197f1c36256b7d
Karin Sifri <karin.sifri@gmail.com>
2025-10-06 19:47:42 +0300

    actually scale with the correct factor this time

--- src/preprocess/scale.py
+++ src/preprocess/scale.py
@@ -1,7 +1,8 @@
+import numpy as np
 import trimesh as tm
 
 
 def set_surface_area_to_one(mesh: tm.Trimesh) -> tm.Trimesh:
     """ Scales a mesh to have a surface area of 1. """
-    mesh.apply_scale(mesh.area)
+    mesh.apply_scale(1 / np.sqrt(mesh.area))
     return mesh
