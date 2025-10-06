72e3828d759a967919bc7bd5072063ccb62db966
Karin Sifri <karin.sifri@gmail.com>
2025-10-06 20:20:45 +0300

    cr fixes

--- src/preprocess/scale.py
+++ src/preprocess/scale.py
@@ -3,6 +3,16 @@ import trimesh as tm
 
 
 def set_surface_area_to_one(mesh: tm.Trimesh) -> tm.Trimesh:
-    """ Scales a mesh to have a surface area of 1. """
+    """ Scales a copy of the mesh to have a surface area of 1.
+
+    Args:
+        mesh: Input mesh to scale.
+
+    Returns:
+        A new mesh with surface area normalized to 1.
+    """
+    if mesh.area <= 0:
+        raise ValueError(f"Mesh has invalid surface area: {mesh.area}. Expected positive value.")
+    mesh = mesh.copy()
     mesh.apply_scale(1 / np.sqrt(mesh.area))
     return mesh
